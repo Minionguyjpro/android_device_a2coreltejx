e744ebc0532ee55f3d827aa868b330f3a8a709e6
Minionguyjpro <minionguyjpro@gmail.com>
2024-08-19 18:45:13 +0200

    Make a few changes, correct names for two a2corelte devices
    Signed-off-by: Minionguyjpro <minionguyjpro@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,3 @@
-DEVICE_PATH := device/samsung/a2corelte
-
 USE_CAMERA_STUB := true
 
 TARGET_NO_BOOTLOADER := true
@@ -28,15 +26,11 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_USES_UNCOMPRESSED_KERNEL := true
-
 BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 # 000RU = recovery kernel, 000KU = system kernel
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --board SRPSB21A012RU
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPSB21A012RU
 
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x002000000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x002600000
@@ -48,10 +42,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_PREBUILT_KERNEL := device/samsung/a2corelte/prebuilt/Image
 TARGET_PREBUILT_DTB := device/samsung/a2corelte/prebuilt/dtb.img
 
-BOARD_KERNEL_SEPARATED_DT := true
-
-TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
-
+# Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
@@ -62,8 +53,6 @@ BOARD_CUSTOM_BOOTIMG_MK :=  device/samsung/a2corelte/bootimg.mk
 PLATFORM_VERSION := 8.1.0
 PLATFORM_SECURITY_PATCH := 2021-10-01
 
-ENABLE_CPUSETS := true
-
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
@@ -72,7 +61,6 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/13600000.usb/13600000.dwc3/gadg
 TW_BRIGHTNESS_PATH := "/sys/devices/14800000.dsim/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 162
-TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_USE_NEW_MINADBD := true
@@ -85,18 +73,17 @@ TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 
 # Encryption support
 TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_USE_SBIN_VOLD := true
-TW_INCLUDE_CRYPTO_SAMSUNG := true
+#TW_INCLUDE_CRYPTO_SAMSUNG := true
 #TARGET_HW_DISK_ENCRYPTION := true
 
-TW_INCLUDE_NTFS_3G := true
-TW_INCLUDE_FUSE_EXFAT := true
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_a2corelte
+TARGET_LIBINIT_DEFINES_FILE := device/samsung/a2corelte/init/init_a2corelte.cpp
+
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += libinit_a2corelte, libicui18n
+RECOVERY_LIBRARY_SOURCE_FILES += $(TARGET_OUT_SHARED_LIBRARIES)/libicui18n.so
 
 # Debug flags
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
-
-# Additional Libraries
-TARGET_RECOVERY_DEVICE_MODULES += libicui18n
-
-RECOVERY_LIBRARY_SOURCE_FILES += $(TARGET_OUT_SHARED_LIBRARIES)/libicui18n.so
