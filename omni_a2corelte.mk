7b0345f65edbf550d451e0aea8b691b4ab9e9035
Minionguyjpro <minionguyjpro@gmail.com>
2024-08-20 13:47:32 +0200

    Update omni_a2corelte.mk

--- omni_a2corelte.mk
+++ omni_a2corelte.mk
@@ -1,8 +1,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := a2corelte
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
