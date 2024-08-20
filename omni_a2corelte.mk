21bda4acbf45e8d61e73f0e94d9a3c7ed04eda6b
Minionguyjpro <minionguyjpro@gmail.com>
2024-08-20 11:59:51 +0200

    Update omni_a2corelte.mk

--- omni_a2corelte.mk
+++ omni_a2corelte.mk
@@ -13,27 +13,6 @@ PRODUCT_PACKAGES += \
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a2corelte
-PRODUCT_MODEL := SM-A260F
 PRODUCT_NAME := omni_a2corelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/samsung/a2corelte/prebuilt/Image
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-ifeq ($(TARGET_PREBUILT_DTB),)
-LOCAL_KERNEL_DTB := device/samsung/a2corelte/prebuilt/dtb.img
-else
-LOCAL_KERNEL_DTB := $(TARGET_PREBUILT_DTB)
-endif
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=a2corelte \
-    PRODUCT_NAME=a2coreltejx \
-    PRIVATE_BUILD_DESC="a2coreltejx-user 8.1.0 OPR6 A260FXXSEAUJ2 release-keys"
-
-BUILD_FINGERPRINT := "samsung/a2coreltejx/a2corelte:8.1.0/OPR6/A260FXXSEAUJ2:user/release-keys"
