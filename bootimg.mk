7162f104990a0b28cf64ec477fd7b0523a032ce9
Minionguyjpro <minionguyjpro@gmail.com>
2024-08-26 21:18:23 +0200

    Update bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -1,16 +1,3 @@
-#####################################################################
-########################## Copyright to : ###########################
-########################## Andreas Blaesius #########################
-############################## LehKeda ##############################
-############################ Dan Pasanen ############################
-#####################################################################
-
-#####################################################################
-########## Compress recovery ramdisk using LZMA #####################
-#####################################################################
-
-LZMA_BIN := $(shell which lzma)
-
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
 ifdef TARGET_PREBUILT_DTB
@@ -21,31 +8,11 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(recovery_ramdisk) \
 		$(recovery_uncompressed_ramdisk) \
 		$(recovery_kernel)
-	export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
-	@echo -e ${CL_CYN}"----- Compressing recovery ramdisk with lzma ------"${CL_RST}
-	rm -f $(recovery_uncompressed_ramdisk).lzma
-	$(LZMA_BIN) $(recovery_uncompressed_ramdisk)
-	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
-	@echo ----- Making recovery image ------
-	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
-	@echo -e ${CL_CYN}"----- Made recovery image -------- $@"${CL_RST}
+	@echo -e ${CL_GRN}"----- Making recovery image ------"${CL_RST}
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(recovery_ramdisk)
+	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
+	@echo -e ${CL_GRN}"----- Lying about SEAndroid state to Samsung bootloader ------"${CL_RST}
+	$(hide) echo -n "SEANDROIDENFORCE" >> $(INSTALLED_RECOVERYIMAGE_TARGET)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(FLASH_IMAGE_TARGET)
-	@echo "------- Made flashable image: $(FLASH_IMAGE_TARGET) -------"
-
-#####################################################################
-######### Compress kernel ramdisk using LZMA LehKeda Edit ###########
-#####################################################################
-
-LZMA_BOOT_RAMDISK := $(PRODUCT_OUT)/ramdisk-lzma.img
-
-$(LZMA_BOOT_RAMDISK): $(BUILT_RAMDISK_TARGET)
-	export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true 
-	gunzip -f < $(BUILT_RAMDISK_TARGET) | lzma -e > $@
-
- $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(LZMA_BOOT_RAMDISK)
-	export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true 
-	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(LZMA_BOOT_RAMDISK)
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
-	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
+	@echo -e ${CL_CYN}"Made Odin flashable recovery tar: ${FLASH_IMAGE_TARGET}"${CL_RST}
