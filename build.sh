0ee7bbf833b8429acc2a013efefd50413bfb0007
Minionguyjpro <minionguyjpro@gmail.com>
2024-12-24 10:02:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 echo Starting build...
 
 crave run  --no-patch -- "rm -rf device/samsung/a2corelte kernel/samsung/a2corelte vendor/samsung/a2corelte; \
-git clone https://github.com/Minionnguyjpro/android_device_samsung_a2corelte --depth 1 -b branchname device/samsung/a2corelte; \
-git clone https://github.com/Minionguyjpro/android_kernel_samsung_a2corelte --depth 1 -b branchname kernel/samsung/a2corelte; \
-git clone https://github.com/Minionguyjpro/android_vendor_samsung_a2corelte --depth 1 -b branchname vendor/samsung/a2corelte; \
+git clone https://github.com/Minionnguyjpro/android_device_samsung_a2corelte --depth 1 -b lineage-21.0 device/samsung/a2corelte; \
+git clone https://github.com/Minionguyjpro/android_kernel_samsung_exynos7870 --depth 1 -b lineage-21.0 kernel/samsung/a2corelte; \
+git clone https://github.com/Minionguyjpro/android_vendor_samsung_a2corelte --depth 1 -b lineage-21.0 vendor/samsung/a2corelte; \
 source build/envsetup.sh; \
 lunch lineage_a2corelte-eng; \
 m bacon"
